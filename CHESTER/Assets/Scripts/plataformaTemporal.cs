a36a8b3b87d0c828727c0a7deb98025ea54a16c2
KaibaKudo <adrian.cortes.4869@gmail.com>
2023-05-03 20:53:20 +0200

    plataforma que se cae al pisarla
    Realizada plataforma que se cae al pisarla despues de X segundos y con una rotación

---
+++ CHESTER/Assets/Scripts/plataformaTemporal.cs
@@ -0,0 +1,51 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class plataformaTemporal : MonoBehaviour
+{
+    [SerializeField] private float tiempoEspera;
+
+    private Rigidbody2D rb;
+
+    [SerializeField] private float velocidadRotacion;
+
+    private bool caida = false;
+    
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void OnCollisionEnter2D(Collision2D col)
+    {
+        if (col.gameObject.CompareTag("Player"))
+        {
+            StartCoroutine(Caida(col));
+        }
+
+        if (col.gameObject.layer == LayerMask.NameToLayer("Ground"))
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    private IEnumerator Caida(Collision2D col)
+    {
+        yield return new WaitForSeconds(tiempoEspera);
+        caida = true;
+        Physics2D.IgnoreCollision(transform.GetComponent<Collider2D>(), col.transform.GetComponent<Collider2D>());
+        rb.constraints = RigidbodyConstraints2D.None;
+        rb.AddForce(new Vector2(0.1f, 0));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (caida)
+        {
+            transform.Rotate(new Vector3(0, 0, -velocidadRotacion * Time.deltaTime));
+        }
+    }
+}
