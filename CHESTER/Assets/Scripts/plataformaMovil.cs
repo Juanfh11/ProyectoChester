8dcfff0cc057cce2aac3e6e3f546337b6bcc584b
KaibaKudo <adrian.cortes.4869@gmail.com>
2023-05-15 02:35:44 +0200

    mapa hecho y plataforma movil realizada
    El escenario por donde se mueve Chester esta hecho, falta por adornarlo y plataforma que se mueve hecha

---
+++ CHESTER/Assets/Scripts/plataformaMovil.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class plataformaMovil : MonoBehaviour
+{
+    public Transform[] pose;
+
+    public float speed;
+
+    public int ID;
+
+    public int suma;
+    // Start is called before the first frame update
+    void Start()
+    {
+        transform.position = pose[0].position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (transform.position == pose[ID].position)
+        {
+            ID += suma;
+        }
+
+        if (ID == pose.Length - 1)
+        {
+            suma = -1;
+        }
+
+        if (ID == 0)
+        {
+            suma = 1;
+        }
+
+        transform.position = Vector3.MoveTowards(transform.position, pose[ID].position, speed * Time.deltaTime);
+    }
+}
