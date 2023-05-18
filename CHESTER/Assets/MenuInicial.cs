123db921ea16c8e71b8e30efbcb464ed69d9ea9b
KaibaKudo <adrian.cortes.4869@gmail.com>
2023-05-18 20:23:22 +0200

    Menu de inicio y menu de opciones creados y funcionales

---
+++ CHESTER/Assets/MenuInicial.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using TMPro;
+
+public class MenuInicial : MonoBehaviour
+{
+
+    public GameObject menuPrincipal;
+    public GameObject menuOpciones;
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        // A cada frame se comprueba si el jugador ha presionado la tecla escape
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            // Si la presiona y está dentro del menú de opciones, este se cierra
+            if (menuOpciones.activeInHierarchy)
+            {
+                menuOpciones.SetActive(false);
+                menuPrincipal.SetActive(true);
+            }
+        }
+    }
+
+    public void Jugar()
+    {
+        // Cargar la escena del juego
+        SceneManager.LoadScene(1);
+    }
+
+    public void Salir()
+    {
+        // Cerrar todo el juego
+        Debug.Log("Saliendo...");
+        Application.Quit();
+    }
+
+}
